60839ae7203c8c1658e3cec31b27eb9831c80a76
Sourav2707 <smartsourav33@gmail.com>
2022-07-15 15:54:29 +0530

    [Sourav] - Checking perfect number

--- LogicalProgram/LogicalProgram/Logic.cs
+++ LogicalProgram/LogicalProgram/Logic.cs
@@ -12,26 +12,25 @@ namespace LogicalProgram
         {
             Console.WriteLine("Enter a number");
             int n = Convert.ToInt32(Console.ReadLine());
-            int sum = 1;
-            for (int i = 2; i * i <= n; i++)
+            int sum = 0;
+            Console.WriteLine($"The divisors for {n} are");
+            for (int i = 1; i <= n / 2; i++)
             {
                 if (n % i == 0)
                 {
-                    if (i * i != n)
-                        sum = sum + i + n / i;
-                    else
-                        sum = sum + i;
+                    Console.WriteLine(i);
+                    sum += i;
                 }
             }
-            // If sum of divisors is equal to n, then n is a perfect number
-            if (sum == n && n != 1)
-            {
-                Console.WriteLine($"The number {n} is a perfect number");
-            }
-            else
-            {
-                Console.WriteLine($"The number {n} is not a perfect number");
+                Console.WriteLine($"{n} = {sum}");
+                if (n == sum)
+                {
+                    Console.WriteLine($"{n} is a perfect number");
+                }
+                else
+                {
+                    Console.WriteLine($"{n} is not a perfect number");
+                }
             }
         }
     }
-}
