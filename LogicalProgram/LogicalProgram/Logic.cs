2e8384dfefc74729d90a6c8cf317b228c25830a2
Sourav2707 <smartsourav33@gmail.com>
2022-07-16 12:36:01 +0530

    [Sourav] - Stopwatch

--- LogicalProgram/LogicalProgram/Logic.cs
+++ LogicalProgram/LogicalProgram/Logic.cs
@@ -2,38 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace LogicalProgram
 {
-    internal class Logic
+    public class Logic
     {
-        public static int random()              //for random numbers from 1 to 20
+        public static void Pro()
         {
-            Random ran = new Random();
-            int r = ran.Next(1, 21);
-            return r;
-        }
-        public static void distinct(int n)          //for distict numbers 
-        {
-            int[] ar = new int[n];
-            for (int i = 0; i < n; i++)             //for storing numbers in an array
-            {
-                ar[i] = random();
-                for (int j = 0; j < i; j++)         //loop from the 0th number to the outer loop index number 
-                {
-                    if (ar[i] == ar[j])             //to compare outer loop number with all the previous numbers
-                    {
-                        ar[i] = random();              //if finds the number equal, stores a random number in it 
-                        i--;              //decreases the outer loop index by one and breaks the inner loop,
-                        break;            // so that after increment in the outer loop same number is checked again for equality 
-                    }
-                }
-            }
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(ar[i] + "  ");
-            }
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("Type start");
+            String start = Console.ReadLine();
+            Console.WriteLine("Started");
+            stopwatch.Start();                      //Start timing
+            Console.WriteLine("Type stop"); 
+            String stop = Console.ReadLine();
+            stopwatch.Stop();                        // Stop timing
+            Console.WriteLine("Stoped");
+            Console.WriteLine("Time Elapsed : " + stopwatch.Elapsed);
+            Console.ReadLine();
         }
     }
 }
--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -1,5 +1,3 @@
 ﻿using LogicalProgram;
-Console.WriteLine("Prints distict coupon numbers from 1 to 20");
-Console.WriteLine("Enter a n for number of distinct numbers");
-int num = Convert.ToInt32(Console.ReadLine());
-Logic.distinct(num);
+Console.WriteLine("Welcome to logical program");
+Logic.Pro();
